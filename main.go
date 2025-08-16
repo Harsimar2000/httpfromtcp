65dd7b92b9ca0793f0cc87362a97e80294419106
Harsimar Rattan <harsimar.rattan@gmail.com>
2025-08-16 15:51:35 -0300

    Feat: creating a channel

--- main.go
+++ main.go
@@ -4,34 +4,50 @@ import (
 	"fmt"
 	"os"
 	"bytes"
+	"io"
 )
 
 func main () {
 	file, err := os.Open("messages.txt")
-	var str string = ""
 
 	if err != nil {
 		fmt.Println("error opening the file")
 	}
+	
+	lines := getLinesChannel(file)
 
-	for  {
-		data := make([]byte, 8)
-		count, err := file.Read(data)
-		data = data[:count]
-
-		if err != nil {
-			return
-		}
-
-		if i:=bytes.IndexByte(data,'\n'); i!=-1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(data)
+	for line := range(lines) {
+		fmt.Printf("read: %s\n", line)
 	}
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	var str string = ""
+
+	go func(){
+		defer close(out)
+		defer file.Close()
+		for  {
+			data := make([]byte, 8)
+			count, err := file.Read(data)
+			data = data[:count]
+
+			if err != nil {
+				return
+			}
+
+			if i:=bytes.IndexByte(data,'\n'); i!=-1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+		if len(str) != 0 {
+			out<-str
+		}
+	} ()
+	return out
 }
